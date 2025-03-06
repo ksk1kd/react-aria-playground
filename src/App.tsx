bf3e9545635f9fd59dd8f86cced3a9594c660848
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-06 23:50:33 +0900

    add select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import {
+  Button,
+  Label,
+  ListBox,
+  ListBoxItem,
+  Popover,
+  Select,
+  SelectValue,
+} from 'react-aria-components'
+
 function App() {
   return (
     <>
-      <div className="container">Contents</div>
+      <div className="container">
+        <section>
+          <h1>Select</h1>
+          <Select>
+            <Label>Favorite Animal</Label>
+            <Button>
+              <SelectValue />
+              <span aria-hidden="true">▼</span>
+            </Button>
+            <Popover>
+              <ListBox>
+                <ListBoxItem>Cat</ListBoxItem>
+                <ListBoxItem>Dog</ListBoxItem>
+                <ListBoxItem>Kangaroo</ListBoxItem>
+              </ListBox>
+            </Popover>
+          </Select>
+        </section>
+      </div>
     </>
   )
 }
